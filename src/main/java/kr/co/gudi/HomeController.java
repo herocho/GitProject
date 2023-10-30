a579d5722f31121bb61f3145a5a6b6b2b8b64b45
cho_young_woong <cho021456@naver.com>
2023-10-30 14:36:55 +0900

    특이사항

---
+++ src/main/java/kr/co/gudi/HomeController.java
@@ -0,0 +1,25 @@
+package kr.co.gudi;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+
+@Controller
+public class HomeController {
+	
+	Logger logger = LoggerFactory.getLogger(getClass());
+
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String home(Model model) {
+		logger.info("Welcome home!");
+		
+		model.addAttribute("msg", "Hello, Git");
+		
+		return "home";
+	}
+	
+}
